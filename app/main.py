dba3bd0cb4d33927772f5f6499868d9a0bc76252
Neal Magee <nem2p@virginia.edu>
2024-10-22 17:12:25 +0000

    Adding devcontainer things

--- app/main.py
+++ app/main.py
@@ -10,8 +10,12 @@ app = FastAPI()
 
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"Good Day": "Sunshine!"}
 
-@app.get("/add/{a}/{b}")
+@app.get("/sum/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+
+@app.get("/multiply/{c}/{d}")
+def multiply(c: int, d: int):
+    return {"product": c * d}
