74d221db83525bee0220cd2ece53386009173a57
Neal Magee <nem2p@virginia.edu>
2024-11-13 09:03:58 -0500

    Updated for DP1

--- app/main.py
+++ app/main.py
@@ -3,8 +3,19 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import mysql.connector
+from mysql.connector import Error
 import json
 import os
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "nem2p"
+
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
+
 
 app = FastAPI()
 
@@ -12,10 +23,34 @@ app = FastAPI()
 def zone_apex():
     return {"Good Day": "Sunshine!"}
 
-@app.get("/sum/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        return None
+    cur.close()
 
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):
-    return {"product": c * d}
+@app.get('/songs')
+def get_genres():
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        print("MySQL Error: ", str(e))
+        return None
+    cur.close()
