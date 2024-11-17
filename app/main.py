e0d3e968854ca34f608b51b057ec002f75347549
taraghose <ghosetara@gmail.com>
2024-11-17 14:26:32 -0500

    Updated fastapi-demoTG file and modified main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+import mysql.connector
+from mysql.connector import Error
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
@@ -8,18 +9,48 @@ import os
 
 app = FastAPI()
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Hello friend!": "Howdy pal!"}
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "admin"
+DBPASS = os.getenv('DBPASS')
+DB = "vwr6nd"
 
-@app.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b} 
+app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/multiply/{c}/{d}")
-def multiply(c: int, d: int):  # Corrected function name and removed extra brace
-    return {"product": c * d}
-
-@app.get("/divide/{g}/{h}")
-def divide(g: int, h: int):  # Removed extra brace
-    return {"quotient": g / h}
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY reid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+    
+@app.get('/songs')
+def get_songs():
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, genres.genre FROM `songs` JOIN genres ON genres.genreid = songs.genre;;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+    
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
+# Connect to the database
