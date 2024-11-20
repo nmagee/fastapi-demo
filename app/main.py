007997b2e6bf1db490fcce64037d7d2edc6eeeba
RyanErm <jph4dg@virginia.edu>
2024-11-20 13:04:38 -0500

    DONE

--- app/main.py
+++ app/main.py
@@ -29,13 +29,15 @@ DBUSER = "admin"
 DBPASS = os.getenv('DBPASS')
 DB = "jph4dg"
 
-db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-cur=db.cursor()
+#db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+#cur=db.cursor()
 
 
 @app.get('/genres')
 def get_genres():
     query = "SELECT * FROM genres ORDER BY genreid;"
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur=db.cursor()
     try:    
         cur.execute(query)
         headers=[x[0] for x in cur.description]
@@ -43,13 +45,19 @@ def get_genres():
         json_data=[]
         for result in results:
             json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
         return(json_data)
     except Error as e:
+        cur.close()
+        db.close()
         return {"Error": "MySQL Error: " + str(e)}
 
 @app.get('/songs')
 def get_songs():
     query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres ON songs.genre = genres.genreid;"
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur=db.cursor()
     try:    
         cur.execute(query)
         headers=[x[0] for x in cur.description]
@@ -58,7 +66,11 @@ def get_songs():
         json_data=[]
         for result in results:
             json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
         return(json_data)
     except Error as e:
+        cur.close()
+        db.close()
         return {"Error": "MySQL Error: " + str(e)}
 
