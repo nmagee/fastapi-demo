b424f7428a9275adeaf77a00bd7738279a63e895
Neal Magee <nem2p@virginia.edu>
2026-03-11 10:50:30 -0400

    Keeping up

--- insert-one.py
+++ insert-one.py
@@ -1,5 +1,10 @@
 import httpx
 
+# This is the equivalent of a CURL request from the command line:
+# curl -X POST -H "Content-Type: application/json" \
+#   -d '{"name":"Myster","email":"mst3k@virginia.edu"}' \
+#   http://localhost:8000/people
+
 person = {'name': 'My Name', 'email': 'abc123@virginia.edu'}
 
 response = httpx.post('http://localhost:8000/people', json=person)
